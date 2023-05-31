32d7dfa35e95b180b1c84705f8194345f65e7098
Toplus8 <juanto101@gmail.com>
2023-05-31 23:30:20 +0200

    some changes

--- src/components/productCard/ProductCard.jsx
+++ src/components/productCard/ProductCard.jsx
@@ -7,7 +7,20 @@ const ProductCard = ({ product, setProducts, productInChart, setProductInChart})
     const buyProduct =(item)=>{
        /*ToDo:Crear un if item exist en productInChart, filter con ! y setProductInCHart con el resultado */
       //console.log(item)
-      setProductInChart(productInChart.concat(item)); 
+      
+      if (!productInChart.includes(item)){
+        setProductInChart(productInChart.concat(item)); 
+        document.getElementById('buy').style.display = "none";
+        document.getElementById('delete').style.display = "block";
+        
+      }else{
+        const updatedChart = productInChart.filter(product => product != item)
+        setProductInChart(updatedChart);
+        document.getElementById('buy').style.display = "block";
+        document.getElementById('delete').style.display = "none";
+        
+      }
+      //ejemplo filter: const resultado = animales.filter(animal => animal != 'oso');
       //console.log(productInChart)
     };
 //console.log(product.id)
@@ -17,7 +30,9 @@ const ProductCard = ({ product, setProducts, productInChart, setProductInChart})
         <div className='imgDiv'><Link to={`/productos/${product.id}`}> <img src={`${product.image}`} ></img> </Link></div>
      
         <div> Precio: { product.price } €</div>
-        <button onClick={()=>buyProduct(product)}>Comprar</button>
+        <button onClick={()=>buyProduct(product)}>
+          <p id='delete' style={{display:"none"}} >Eliminar</p><p id='buy'>Comprar</p>
+          </button>
     </div>
   )
 }
